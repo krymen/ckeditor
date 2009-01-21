0d9309c7280a59a2203a05c03e7628580b1ef3b7
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-01-21 15:41:39 +0000

    Aligned the trunk with recent changes made to the prototype.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@2981 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/editor.js
+++ _source/core/editor.js
@@ -424,25 +424,21 @@ CKEDITOR.tools.extend( CKEDITOR.editor.prototype,
 		 */
 		insertHtml : function( data )
 		{
-			var eventData = { insert : data };
-			this.fire( 'insertHtml', eventData );
-			this.fire( 'afterInsertHtml', eventData );
-			this._.data = this.document.getBody().getHtml();
+			this.fire( 'insertHtml', data );
 		},
 
 		/**
-		 * Inserts an element into the currently selected position in the editor.
-		 * @param {CKEDITOR.dom.element} element Element to be inserted into the editor.
+		 * Inserts an element into the currently selected position in the
+		 * editor.
+		 * @param {CKEDITOR.dom.element} element The element to be inserted
+		 *		into the editor.
 		 * @example
-		 * var element = CKEDITOR.dom.element.createFromHtml( '<img src="hello.png" border="0" title="Hello" />' );
+		 * var element = CKEDITOR.dom.element.createFromHtml( '&lt;img src="hello.png" border="0" title="Hello" /&gt;' );
 		 * CKEDITOR.instances.editor1.<b>insertElement( element )</b>;
 		 */
 		insertElement : function( element )
 		{
-			var eventData = { insert : element };
-			this.fire( 'insertElement', eventData );
-			this.fire( 'afterInsertElement', eventData );
-			this._.data = this.document.getBody().getHtml();
+			this.fire( 'insertElement', element );
 		},
 
 		/**
