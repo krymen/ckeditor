eee39ca030f81b705dc29bd60fc53f41f733f409
arczi <arczi@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-03-04 08:15:32 +0000

    #2986 Fixed onLoad lockRatio button bug.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3122 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/image/dialogs/image.js
+++ _source/plugins/image/dialogs/image.js
@@ -617,7 +617,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 															resetButton.on( 'click', function()
 																{
 																	resetSize( this );
-																}, this );	//this = dialog.
+																}, this.getDialog() );
 															resetButton.on( 'mouseover', function()
 																{
 																	this.addClass( 'BtnOver' );
@@ -645,7 +645,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 																			updatePreview( this );
 																		}
 																	}
-																}, this );
+																}, this.getDialog() );
 															ratioButton.on( 'mouseover', function()
 																{
 																	this.addClass( 'BtnOver' );
