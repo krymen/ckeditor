dbf9b6c787245f73440d44fb3ce814f055a19cd4
martinkou <martinkou@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-03-27 22:43:18 +0000

    Fixed #3213 : Changed the voice readout of CKEditor in JAWS to "Rich Text Editor".
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3306 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/wysiwygarea/plugin.js
+++ _source/plugins/wysiwygarea/plugin.js
@@ -154,7 +154,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 							mainElement.setAttributes(
 								{
 									role : 'region',
-									title : 'CKEditor ' + editor.name + '. Type in text.'
+									title : editor.lang.editorTitle.replace( '%1', editor.name ) + '. Type in text.'
 								} );
 							iframe.setAttributes(
 								{
@@ -171,7 +171,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 								'top' : '-1000000px',
 								left : '-1000000px'
 							} );
-							label.append( CKEDITOR.document.createText( 'CKEditor ' + editor.name ) );
+							label.append( CKEDITOR.document.createText( editor.lang.editorTitle.replace( '%1', editor.name ) ) );
 							label.insertBefore( iframe );
 						}
 					};
