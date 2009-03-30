0c84ab13c1b8b18b5c5968b155058969ceb09929
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-03-30 07:22:07 +0000

    #3091 : Fixed the style system for cross block inline styles applied.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3311 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/dom/element.js
+++ _source/core/dom/element.js
@@ -987,7 +987,10 @@ CKEDITOR.tools.extend( CKEDITOR.dom.element.prototype,
 		 */
 		removeStyle : function( name )
 		{
-			this.setStyle( name, '' );
+			if ( this.$.style.removeAttribute )
+				this.$.style.removeAttribute( CKEDITOR.tools.cssStyleToDomStyle( name ) );
+			else
+				this.setStyle( name, '' );
 
 			if ( !this.$.style.cssText )
 				this.removeAttribute( 'style' );
