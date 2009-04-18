339b118ab20022f299813305887e5253d5c1be1d
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-04-18 09:29:37 +0000

    Fix #3344: Add document end protection logic when traversing.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3395 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/styles/plugin.js
+++ _source/plugins/styles/plugin.js
@@ -316,7 +316,13 @@ CKEDITOR.STYLE_OBJECT = 3;
 		var boundaryNodes = range.getBoundaryNodes();
 		var firstNode = boundaryNodes.startNode;
 		var lastNode = boundaryNodes.endNode.getNextSourceNode( true );
-
+		
+		// Probably the document end is reached, we need a marker node.  
+		if ( !lastNode )
+		{
+				lastNode = document.createText( '' );
+				lastNode.insertAfter( range.endContainer );
+		}
 		// The detection algorithm below skips the contents inside bookmark nodes, so
 		// we'll need to make sure lastNode isn't the &nbsp; inside a bookmark node.
 		var lastParent = lastNode.getParent();
