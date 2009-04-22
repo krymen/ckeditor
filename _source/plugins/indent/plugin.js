4f6f07a68fb1e29a4098ddcb45c71c0eef608fc6
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-04-22 15:20:48 +0000

    Fix #3398: logic error when detecting 'nearestListBlock'.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3415 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/indent/plugin.js
+++ _source/plugins/indent/plugin.js
@@ -235,7 +235,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			var bookmarks = selection.createBookmarks( true ),
 				nearestListBlock = range.getCommonAncestor();
 
-			while ( !( nearestListBlock.type == CKEDITOR.NODE_ELEMENT && 
+			while ( nearestListBlock && !( nearestListBlock.type == CKEDITOR.NODE_ELEMENT && 
 				listNodeNames[ nearestListBlock.getName() ] ) )
 				nearestListBlock = nearestListBlock.getParent();
 
