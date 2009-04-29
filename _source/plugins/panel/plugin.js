5dd07a909d29c036e99bd765ccca391f1a682e8d
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-04-29 08:57:33 +0000

    #3031 : The floating panels where not working in FF2.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3443 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/panel/plugin.js
+++ _source/plugins/panel/plugin.js
@@ -143,11 +143,14 @@ CKEDITOR.ui.panel.prototype =
 					'<!DOCTYPE html>' +
 					'<html dir="' + dir + '" class="' + className + '_container">' +
 						'<head>' +
-							'<link type="text/css" rel=stylesheet href="' + this.css.join( '"><link type="text/css" rel="stylesheet" href="' ) + '">' +
 							'<style>.' + className + '_container{visibility:hidden}</style>' +
 						'</head>' +
 						'<body class="cke_' + dir + ' cke_panel_frame" style="margin:0;padding:0">' +
 						'</body>' +
+						// It looks strange, but for FF2, the styles must go
+						// after <body>, so it (body) becames immediatelly
+						// available. (#3031)
+						'<link type="text/css" rel=stylesheet href="' + this.css.join( '"><link type="text/css" rel="stylesheet" href="' ) + '">' +
 					'<\/html>' );
 				doc.$.close();
 
