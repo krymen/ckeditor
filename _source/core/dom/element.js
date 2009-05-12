058859ddb8e2f2f1c23eb69d1ef8c412f9074f31
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-05-12 11:37:53 +0000

    Fix #3473: Improve bogus BR logic.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3493 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/dom/element.js
+++ _source/core/dom/element.js
@@ -241,13 +241,12 @@ CKEDITOR.tools.extend( CKEDITOR.dom.element.prototype,
 			// Ignore empty/spaces text.
 			while ( lastChild && lastChild.type == CKEDITOR.NODE_TEXT && !CKEDITOR.tools.rtrim( lastChild.getText() ) )
 				lastChild = lastChild.getPrevious();
-
-			if ( !lastChild || ( lastChild.is && ( !lastChild.is( 'br' ) || !lastChild.getAttribute( '_cke_bogus' ) ) ) )
+			if ( !lastChild || !lastChild.is || !lastChild.is( 'br' ) )
 			{
 				this.append(
 					CKEDITOR.env.opera ?
 						this.getDocument().createText('') :
-						this.getDocument().createElement( 'br', { attributes : { _cke_bogus : 1 } } ) );
+						this.getDocument().createElement( 'br' ) );
 			}
 		},
 
