a3327183b89fd9b7444e4445961c9999ca3b8758
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-06-24 09:49:02 +0000

    #3819 : The cursor was not visible when applying style to collapsed selections in Firefox 2.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3770 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/selection/plugin.js
+++ _source/plugins/selection/plugin.js
@@ -855,7 +855,20 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				{
 					var range = ranges[ i ];
 					var nativeRange = this.document.$.createRange();
-					nativeRange.setStart( range.startContainer.$, range.startOffset );
+					var startContainer = range.startContainer;
+
+					// In FF2, if we have a collapsed range, inside an empty
+					// element, we must add something to it otherwise the caret
+					// will not be visible.
+					if ( range.collapsed &&
+						( CKEDITOR.env.gecko && CKEDITOR.env.version < 10900 ) &&
+						startContainer.type == CKEDITOR.NODE_ELEMENT &&
+						!startContainer.getChildCount() )
+					{
+						startContainer.appendText( '' );
+					}
+
+					nativeRange.setStart( startContainer.$, range.startOffset );
 					nativeRange.setEnd( range.endContainer.$, range.endOffset );
 
 					// Select the range.
