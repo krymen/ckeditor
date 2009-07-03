b4e049f9e9ffd7c55c3247f3be375f6cbc6fe452
martinkou <martinkou@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-07-03 06:40:41 +0000

    Fixed #3849 : Fixed IE8 crashes when applying lists and indenting.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3808 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/selection/plugin.js
+++ _source/plugins/selection/plugin.js
@@ -943,7 +943,7 @@ CKEDITOR.dom.range.prototype.select =
 		function()
 		{
 			var collapsed = this.collapsed;
-			var isStartMakerAlone;
+			var isStartMarkerAlone;
 			var dummySpan;
 
 			var bookmark = this.createBookmark();
@@ -976,19 +976,11 @@ CKEDITOR.dom.range.prototype.select =
 			}
 			else
 			{
-// The isStartMakerAlone logic comes from V2. It guarantees that the lines
-// will expand and that the cursor will be blinking on the right place.
-// Actually, we are using this flag just to avoid using this hack in all
-// situations, but just on those needed.
-
-// But, in V3, somehow it is not interested on working whe hitting SHIFT+ENTER
-// inside text. So, let's jsut leave the hack happen always.
-
-// I'm still leaving the code here just in case. We may find some other IE
-// weirdness and uncommenting this stuff may be useful.
-
-//				isStartMakerAlone = ( !startNode.hasPrevious() || ( startNode.getPrevious().is && startNode.getPrevious().is( 'br' ) ) )
-//					&& !startNode.hasNext();
+				// The isStartMarkerAlone logic comes from V2. It guarantees that the lines
+				// will expand and that the cursor will be blinking on the right place.
+				// Actually, we are using this flag just to avoid using this hack in all
+				// situations, but just on those needed.
+				isStartMarkerAlone = !startNode.hasPrevious() || ( startNode.getPrevious().is && startNode.getPrevious().is( 'br' ) );
 
 				// Append a temporary <span>&#65279;</span> before the selection.
 				// This is needed to avoid IE destroying selections inside empty
@@ -999,14 +991,14 @@ CKEDITOR.dom.range.prototype.select =
 				dummySpan.setHtml( '&#65279;' );	// Zero Width No-Break Space (U+FEFF). See #1359.
 				dummySpan.insertBefore( startNode );
 
-//				if ( isStartMakerAlone )
-//				{
+				if ( isStartMarkerAlone )
+				{
 					// To expand empty blocks or line spaces after <br>, we need
 					// instead to have any char, which will be later deleted using the
 					// selection.
 					// \ufeff = Zero Width No-Break Space (U+FEFF). (#1359)
 					this.document.createText( '\ufeff' ).insertBefore( startNode );
-//				}
+				}
 			}
 
 			// Remove the markers (reset the position, because of the changes in the DOM tree).
@@ -1015,8 +1007,8 @@ CKEDITOR.dom.range.prototype.select =
 
 			if ( collapsed )
 			{
-//				if ( isStartMakerAlone )
-//				{
+				if ( isStartMarkerAlone )
+				{
 					// Move the selection start to include the temporary \ufeff.
 					ieRange.moveStart( 'character', -1 );
 
@@ -1024,9 +1016,9 @@ CKEDITOR.dom.range.prototype.select =
 
 					// Remove our temporary stuff.
 					this.document.$.selection.clear();
-//				}
-//				else
-//					ieRange.select();
+				}
+				else
+					ieRange.select();
 
 				dummySpan.remove();
 			}
