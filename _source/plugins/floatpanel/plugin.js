62ebc31ed7c54c41935b5f7eb5c99a7e8cce8f94
tobiasz.cudnik <tobiasz.cudnik@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-07-09 07:35:46 +0000

    Fixed #3879 - [webkit] Color button panel incorrect size on first open.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@3843 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/floatpanel/plugin.js
+++ _source/plugins/floatpanel/plugin.js
@@ -211,15 +211,8 @@ CKEDITOR.plugins.add( 'floatpanel',
 								panel._.currentBlock.element.setStyle( 'display', 'none' ).removeStyle( 'display' );
 							}
 
-							if ( !CKEDITOR.env.gecko || panel.isLoaded )
-							{
-								// IE7 needs some time (setting the delay to 0ms won't work) to refresh
-								// the scrollHeight. (#3174)
-								if ( CKEDITOR.env.ie && CKEDITOR.env.version >= 7 )
-									setTimeout( setHeight, 50 );
-								else
-									setHeight();
-							}
+							if ( panel.isLoaded )
+								setHeight();
 							else
 								panel.onLoad = setHeight;
 						}
