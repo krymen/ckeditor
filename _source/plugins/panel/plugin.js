ba4a0216c8ebca259d1011312f1d7f5f4b1742fd
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-08-03 14:22:02 +0000

    Fixing #4186 : [FF] First open float panel cause host page scrollbar blinking.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@4051 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/panel/plugin.js
+++ _source/plugins/panel/plugin.js
@@ -78,7 +78,8 @@ CKEDITOR.ui.panel.prototype =
 		output.push(
 			'<div class="', editor.skinClass ,'"' +
 				' lang="', editor.langCode, '"' +
-				' style="z-index:' + ( editor.config.baseFloatZIndex + 1 ) + '">' +
+				// iframe loading need sometime, keep the panel hidden(#4186).
+				' style="display:none;z-index:' + ( editor.config.baseFloatZIndex + 1 ) + '">' +
 				'<div' +
 					' id=', id,
 					' dir=', editor.lang.dir,
