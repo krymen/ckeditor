132e540220026cc73ab76cf8784b58c97d314a53
tobiasz.cudnik <tobiasz.cudnik@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-08-18 09:45:41 +0000

    Fixed #4219 - Fallback mechanism needed for config.language.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@4135 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/lang.js
+++ _source/core/lang.js
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
 Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -92,8 +92,10 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 		 */
 		load : function( languageCode, defaultLanguage, callback )
 		{
-			if ( !languageCode )
-				languageCode = this.detect( defaultLanguage );
+			// If no languageCode - fallback to browser or default.
+			// If languageCode - fallback to no-localized version or default.
+			if ( !languageCode || !CKEDITOR.lang[ languageCode ] )
+				languageCode = this.detect( defaultLanguage, languageCode );
 
 			if ( !this[ languageCode ] )
 			{
@@ -122,11 +124,12 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 		 * @example
 		 * alert( CKEDITOR.lang.detect( 'en' ) );  // e.g., in a German browser: "de"
 		 */
-		detect : function( defaultLanguage )
+		detect : function( defaultLanguage, probeLanguage )
 		{
 			var languages = this.languages;
+			probeLanguage = probeLanguage || navigator.userLanguage || navigator.language;
 
-			var parts = ( navigator.userLanguage || navigator.language )
+			var parts = probeLanguage
 					.toLowerCase()
 					.match( /([a-z]+)(?:-([a-z]+))?/ ),
 				lang = parts[1],
