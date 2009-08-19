70fc70a6257352d39d2440e8a255772248022e02
tobiasz.cudnik <tobiasz.cudnik@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-08-19 10:06:17 +0000

    Postfix for #4028 - Maximize control's tool tip is wrong once it is maximized.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@4139 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/ui.js
+++ _source/core/ui.js
@@ -67,7 +67,7 @@ CKEDITOR.ui.prototype =
 	{
 		var item	= this._.items[ name ],
 			handler	= item && this._.handlers[ item.type ],
-			command = item.command && this._.editor.getCommand( item.command );
+			command = item && item.command && this._.editor.getCommand( item.command );
 
 		var result = handler && handler.create.apply( this, item.args );
 
