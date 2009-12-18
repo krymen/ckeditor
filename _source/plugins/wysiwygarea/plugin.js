b2fb0da716cf25c41021d798bf9aabd7bc1fa308
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-12-18 10:40:48 +0000

    #4848 : Fixed the focus method on IE.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@4755 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/wysiwygarea/plugin.js
+++ _source/plugins/wysiwygarea/plugin.js
@@ -661,6 +661,21 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 								else if ( editor.window )
 								{
 									editor.window.focus();
+
+									// Force the selection to happen, in this way
+									// we guarantee the focus will be there. (#4848)
+									if ( CKEDITOR.env.ie )
+									{
+										var sel = editor.getSelection();
+										sel = sel && sel.getNative();
+										var range = sel && sel.type && sel.createRange();
+										if ( range )
+										{
+											sel.empty();
+											range.select();
+										}
+									}
+
 									editor.selectionChange();
 								}
 							}
