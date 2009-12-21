7117c4ed338086c18dbe4997e007fd5720952a68
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-12-21 15:33:54 +0000

    #4837 : Further updates to the Finnish language file.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@4781 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/lang/fi.js
+++ _source/lang/fi.js
@@ -692,6 +692,6 @@ CKEDITOR.lang['fi'] =
 		clear : 'Poista'
 	},
 
-	toolbarCollapse : 'Collapse Toolbar', // MISSING
-	toolbarExpand : 'Expand Toolbar' // MISSING
+	toolbarCollapse : 'Kutista työkalupalkki',
+	toolbarExpand : 'Laajenna työkalupalkki'
 };
