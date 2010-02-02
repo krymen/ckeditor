60a713c65512d0703abe5de8104455f3f83dba81
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-02-02 06:19:31 +0000

    #4683: Manually escape double-quote into corresponding character entities (&quot;).
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5007 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/htmlwriter/plugin.js
+++ _source/plugins/htmlwriter/plugin.js
@@ -174,6 +174,9 @@ CKEDITOR.htmlWriter = CKEDITOR.tools.createClass(
 			if ( this.forceSimpleAmpersand )
 				attValue = attValue.replace( /&amp;/, '&' );
 
+			// Browsers don't always escape quote in attribute values. (#4683) 
+			attValue = attValue.replace( /"/g, '&quot;' )
+
 			this._.output.push( ' ', attName, '="', attValue, '"' );
 		},
 
