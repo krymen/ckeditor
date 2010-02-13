fa4be6f6d58ea1a48eb372deef69d6a0e3a35f60
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-02-13 09:43:49 +0000

    #5135 : The TAB key will now behave properly when in Source mode.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5116 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/tab/plugin.js
+++ _source/plugins/tab/plugin.js
@@ -5,6 +5,12 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 (function()
 {
+	var meta =
+	{
+		editorFocus : false,
+		modes : { wysiwyg:1, source:1 }
+	};
+
 	var blurCommand =
 		{
 			exec : function( editor )
@@ -39,7 +45,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				tabText += '\xa0';
 
 			// Register the "tab" and "shiftTab" commands.
-			editor.addCommand( 'tab',
+			editor.addCommand( 'tab', CKEDITOR.tools.extend(
 				{
 					exec : function( editor )
 					{
@@ -61,9 +67,9 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 						return true;
 					}
-				});
+				}, meta ) );
 
-			editor.addCommand( 'shiftTab',
+			editor.addCommand( 'shiftTab', CKEDITOR.tools.extend(
 				{
 					exec : function( editor )
 					{
@@ -74,10 +80,10 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 						return true;
 					}
-				});
+				}, meta ) );
 
-			editor.addCommand( 'blur', blurCommand );
-			editor.addCommand( 'blurBack', blurBackCommand );
+			editor.addCommand( 'blur', CKEDITOR.tools.extend( blurCommand, meta ) );
+			editor.addCommand( 'blurBack', CKEDITOR.tools.extend( blurBackCommand, meta ) );
 		}
 	});
 })();
