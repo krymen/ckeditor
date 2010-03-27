242b69774c7e9e3f95a60fb755a7042e6d3bfcd7
alfonsoml <alfonsoml@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-03-27 16:08:20 +0000

    Correct redeclaration of stylesText introduced in [5291]
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5303 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/styles/plugin.js
+++ _source/plugins/styles/plugin.js
@@ -321,13 +321,13 @@ CKEDITOR.STYLE_OBJECT = 3;
 		for ( var style in stylesDef )
 		{
 			var styleVal = stylesDef[ style ],
-					stylesText = ( style + ':' + styleVal ).replace( semicolonFixRegex, ';' );
+					text = ( style + ':' + styleVal ).replace( semicolonFixRegex, ';' );
 
-			// Some browsers don't support 'inheirt' property value, leave them intact. (#5242)
+			// Some browsers don't support 'inherit' property value, leave them intact. (#5242)
 			if ( styleVal == 'inherit' )
-				specialStylesText += stylesText;
+				specialStylesText += text;
 			else
-				stylesText += stylesText;
+				stylesText += text;
 		}
 
 		// Browsers make some changes to the style when applying them. So, here
@@ -1234,7 +1234,7 @@ CKEDITOR.STYLE_OBJECT = 3;
 		temp [ isStyle ? 'setStyle' : 'setAttribute' ]( name, value );
 		return temp[ isStyle ? 'getStyle' : 'getAttribute' ]( name );
 	}
-	
+
 	function normalizeCssText( unparsedCssText, nativeNormalize )
 	{
 		var styleText;
