cc6cb27fcd4a95635b4f9ed5f91f25ef0274d3e2
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-03-31 16:09:02 +0000

    Fixing a regression of [5284] where control type selection is changed into text selection.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5325 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/wysiwygarea/plugin.js
+++ _source/plugins/wysiwygarea/plugin.js
@@ -223,6 +223,11 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 					fixedBlock.remove();
 				}
 			}
+			
+			range.select();
+			// Notify non-IE that selection has changed.
+			if ( !CKEDITOR.env.ie )
+				editor.selectionChange();
 		}
 
 		// All browsers are incapable to moving cursor out of certain non-exitable
@@ -254,9 +259,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			if ( !CKEDITOR.env.ie )
 				paddingBlock.appendBogus();
 		}
-
-		range.select();
-		editor.selectionChange();
 	}
 
 	CKEDITOR.plugins.add( 'wysiwygarea',
