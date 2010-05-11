4b9e32857b897c66c7fd08c34aaf464916f99198
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-05-11 11:01:26 +0000

    #5618 : Fixed a regression that's breaking the recreation of editor instances.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5448 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/dialog/plugin.js
+++ _source/plugins/dialog/plugin.js
@@ -2786,7 +2786,7 @@ CKEDITOR.DIALOG_RESIZE_BOTH = 3;
 			dialogs[ name ].destroy();
 
 		// Remove dialog cover on last instance destroy.
-		if ( CKEDITOR.tools.isEmpty( CKEDITOR.instances ) );
+		if ( CKEDITOR.tools.isEmpty( CKEDITOR.instances ) && coverElement )
 			coverElement.remove();
 	});
 
