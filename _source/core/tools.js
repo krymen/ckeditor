c4ce9046b5c216a45e6469132cd9ece1ccdfc96f
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-06-22 03:51:29 +0000

    Better inline documentation.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5630 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/tools.js
+++ _source/core/tools.js
@@ -220,6 +220,10 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			return ( !!object && object instanceof Array );
 		},
 
+		/**
+		 * Whether the object contains no properties of it's own.
+ 		 * @param object
+		 */
 		isEmpty : function ( object )
 		{
 			for ( var i in object )
@@ -229,6 +233,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			}
 			return true;
 		},
+
 		/**
 		 * Transforms a CSS property name to its relative DOM style name.
 		 * @param {String} cssName The CSS property name.
@@ -668,6 +673,10 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			return fn && fn.apply( window, Array.prototype.slice.call( arguments, 1 ) );
 		},
 
+		/**
+		 * Append the 'px' length unit to the size if it's missing.
+		 * @param length
+		 */
 		cssLength : (function()
 		{
 			var decimalRegex = /^\d+(?:\.\d+)?$/;
@@ -677,11 +686,20 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			};
 		})(),
 
+		/**
+		 * String specified by {@param str} repeats {@param times} times.
+		 * @param str
+		 * @param times
+		 */
 		repeat : function( str, times )
 		{
 			return new Array( times + 1 ).join( str );
 		},
 
+		/**
+		 * Return the first successfully executed function's return value that
+		 * doesn't throw any exception.
+		 */
 		tryThese : function()
 		{
 			var returnValue;
