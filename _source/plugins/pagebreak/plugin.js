25d3dee721b3e03f5f123e6a4d071fa7fb65c0ac
tobiasz.cudnik <tobiasz.cudnik@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-07-12 09:52:56 +0000

    Fixed #5079 Page break in lists move to above the list when you switch from WYSIWYG to HTML mode and back.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5697 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/pagebreak/plugin.js
+++ _source/plugins/pagebreak/plugin.js
@@ -30,9 +30,8 @@ CKEDITOR.plugins.add( 'pagebreak',
 				'background-position: center center;' +
 				'background-repeat: no-repeat;' +
 				'clear: both;' +
-				'display: block;' +
 				'float: none;' +
-				'width:100% !important; _width:99.9% !important;' +
+				'width: 5em;' +
 				'border-top: #999999 1px dotted;' +
 				'border-bottom: #999999 1px dotted;' +
 				'height: 5px !important;' +
@@ -54,7 +53,7 @@ CKEDITOR.plugins.add( 'pagebreak',
 				{
 					elements :
 					{
-						div : function( element )
+						span : function( element )
 						{
 							var attributes = element.attributes,
 								style = attributes && attributes.style,
@@ -62,7 +61,7 @@ CKEDITOR.plugins.add( 'pagebreak',
 								childStyle = child && ( child.name == 'span' ) && child.attributes.style;
 
 							if ( childStyle && ( /page-break-after\s*:\s*always/i ).test( style ) && ( /display\s*:\s*none/i ).test( childStyle ) )
-								return editor.createFakeParserElement( element, 'cke_pagebreak', 'div' );
+								return editor.createFakeParserElement( element, 'cke_pagebreak', 'span' );
 						}
 					}
 				});
@@ -77,10 +76,10 @@ CKEDITOR.plugins.pagebreakCmd =
 	exec : function( editor )
 	{
 		// Create the element that represents a print break.
-		var breakObject = CKEDITOR.dom.element.createFromHtml( '<div style="page-break-after: always;"><span style="display: none;">&nbsp;</span></div>' );
+		var breakObject = CKEDITOR.dom.element.createFromHtml( '<span style="page-break-after: always;"><span style="display: none;">&nbsp;</span></span>' );
 
 		// Creates the fake image used for this element.
-		breakObject = editor.createFakeElement( breakObject, 'cke_pagebreak', 'div' );
+		breakObject = editor.createFakeElement( breakObject, 'cke_pagebreak', 'span' );
 
 		var ranges = editor.getSelection().getRanges();
 
@@ -93,7 +92,6 @@ CKEDITOR.plugins.pagebreakCmd =
 			if ( i > 0 )
 				breakObject = breakObject.clone( true );
 
-			range.splitBlock( 'p' );
 			range.insertNode( breakObject );
 			if ( i == ranges.length - 1 )
 			{
