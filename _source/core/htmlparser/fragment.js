9dc69857353509b805d0d37f45671e6409a48b00
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-10-18 07:28:05 +0000

    We should keep inline element when parsing that contains only comments.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@5946 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/htmlparser/fragment.js
+++ _source/core/htmlparser/fragment.js
@@ -386,6 +386,7 @@ CKEDITOR.htmlParser.fragment = function()
 
 		parser.onComment = function( comment )
 		{
+			checkPending();
 			currentNode.add( new CKEDITOR.htmlParser.comment( comment ) );
 		};
 
