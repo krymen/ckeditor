cf7327df510069d95b7f1d5be8e02649dada405d
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-12-23 12:22:25 +0000

    #6652: A continual clean up fix of #6621.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@6272 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/styles/plugin.js
+++ _source/plugins/styles/plugin.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -1422,23 +1422,17 @@ CKEDITOR.STYLE_OBJECT = 3;
 	function applyStyle( document, remove )
 	{
 		var selection = document.getSelection(),
-			// Bookmark the range so we can re-select it after processing.
-			bookmarks = selection.createBookmarks( 1 ),
-			ranges = selection.getRanges(),
+			ranges = selection.getRanges( true ),
 			func = remove ? this.removeFromRange : this.applyToRange,
 			range;
 
 		var iterator = ranges.createIterator();
 		while ( ( range = iterator.getNextRange() ) )
-			func.call( this, range );
-
-		if ( bookmarks.length == 1 && bookmarks[0].collapsed )
 		{
-			selection.selectRanges( ranges );
-			document.getById( bookmarks[ 0 ].startNode ).remove();
+			func.call( this, range );
 		}
-		else
-			selection.selectBookmarks( bookmarks );
+
+		selection.selectRanges( ranges );
 	}
 })();
 
