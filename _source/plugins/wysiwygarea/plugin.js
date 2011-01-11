4dddeec2e861b43f908e9eb204f394a30b7b7a85
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-01-11 14:37:36 +0000

    #6703: editor::focus event is not fired on dialog close.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@6306 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/wysiwygarea/plugin.js
+++ _source/plugins/wysiwygarea/plugin.js
@@ -708,14 +708,15 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 							} );
 						}
 
-						domWindow.on( 'blur', function()
+						var focusTarget = CKEDITOR.env.ie ? iframe : domWindow;
+						focusTarget.on( 'blur', function()
 							{
 								editor.focusManager.blur();
 							});
 
 						var wasFocused;
 
-						domWindow.on( 'focus', function()
+						focusTarget.on( 'focus', function()
 							{
 								var doc = editor.document;
 
