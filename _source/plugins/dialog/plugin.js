253e6b8c344f650f62e698fc22d3bbb1a1cd9845
Saare <Saare@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-02-07 13:01:10 +0000

    Cover is now hidden when one tries to open a dialog that does not exist.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@6407 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/dialog/plugin.js
+++ _source/plugins/dialog/plugin.js
@@ -2957,7 +2957,10 @@ CKEDITOR.DIALOG_RESIZE_BOTH = 3;
 					return dialog;
 				}
 				else if ( dialogDefinitions == 'failed' )
+				{
+					hideCover();
 					throw new Error( '[CKEDITOR.dialog.openDialog] Dialog "' + dialogName + '" failed when loading definition.' );
+				}
 
 				var me = this;
 
