561ec5df6fce6df5e0e1c387524e2cd9d6754e47
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-05-19 16:18:34 +0000

    #7323: Fixing dialog field centre alignment on IE quirks.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@6942 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/liststyle/dialogs/liststyle.js
+++ _source/plugins/liststyle/dialogs/liststyle.js
@@ -46,7 +46,8 @@
 								type : 'select',
 								label : lang.type,
 								id : 'type',
-								style : 'width: 150px; margin: auto;',
+								align : 'center',
+								style : 'width:150px',
 								items :
 								[
 									[ lang.notset, '' ],
