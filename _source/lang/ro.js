fb0ba274fc6398e16963bfb3647a5b8763749ecc
Anna <Anna@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-08-22 12:11:42 +0000

    #7948 : Tooltips with information about correct length units are now displayed for the width/height fields of the Table Properties dialog window.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7215 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/lang/ro.js
+++ _source/lang/ro.js
@@ -14,7 +14,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 /**
- * Constains the dictionary of language entries.
+ * Contains the dictionary of language entries.
  * @namespace
  */
 CKEDITOR.lang['ro'] =
@@ -124,6 +124,7 @@ CKEDITOR.lang['ro'] =
 		invalidCssLength	: 'Value specified for the "%1" field must be a positive number with or without a valid CSS measurement unit (px, %, in, cm, mm, em, ex, pt, or pc).', // MISSING
 		invalidHtmlLength	: 'Value specified for the "%1" field must be a positive number with or without a valid HTML measurement unit (px or %).', // MISSING
 		invalidInlineStyle	: 'Value specified for the inline style must consist of one or more tuples with the format of "name : value", separated by semi-colons.', // MISSING
+		cssLengthTooltip	: 'Enter a number for a value in pixels or a number with a valid CSS unit (px, %, in, cm, mm, em, ex, pt, or pc).', // MISSING
 
 		// Put the voice-only part of the label in the span.
 		unavailable		: '%1<span class="cke_accessibility">, unavailable</span>' // MISSING
@@ -259,8 +260,6 @@ CKEDITOR.lang['ro'] =
 		rows		: 'Linii',
 		columns		: 'Coloane',
 		border		: 'Mărimea marginii',
-		widthTitle : 'Table width specified with or without a valid CSS measurement unit (px, %, in, cm, mm, em, ex, pt, or pc).', // MISSING
-		heightTitle : 'Table height specified with or without a valid CSS measurement unit (px, %, in, cm, mm, em, ex, pt, or pc).', // MISSING
 		widthPx		: 'pixeli',
 		widthPc		: 'procente',
 		widthUnit	: 'width unit', // MISSING
