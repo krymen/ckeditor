142d5acf9f173e89ca9542523c08609dc9146ea4
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-08-24 15:06:34 +0000

    #8222: Avoid wrong selection lock with the fix body logic.
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7225 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/wysiwygarea/plugin.js
+++ _source/plugins/wysiwygarea/plugin.js
@@ -398,8 +398,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 					&& !pathBlock.is( 'pre' )
 					&& !pathBlock.getBogus() )
 			{
-				editor.fire( 'updateSnapshot' );
-				restoreDirty( editor );
 				pathBlock.appendBogus();
 			}
 		}
@@ -412,10 +410,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				&& blockLimit.getName() == 'body'
 				&& !path.block )
 		{
-			editor.fire( 'updateSnapshot' );
-			restoreDirty( editor );
-			CKEDITOR.env.ie && restoreSelection( selection );
-
 			var fixedBlock = range.fixBlock( true,
 					editor.config.enterMode == CKEDITOR.ENTER_DIV ? 'div' : 'p'  );
 
@@ -464,10 +458,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 		var testPath = new CKEDITOR.dom.elementPath( testRange.startContainer );
 		if ( !testPath.blockLimit.is( 'body') )
 		{
-			editor.fire( 'updateSnapshot' );
-			restoreDirty( editor );
-			CKEDITOR.env.ie && restoreSelection( selection );
-
 			var paddingBlock;
 			if ( enterMode != CKEDITOR.ENTER_BR )
 				paddingBlock = body.append( editor.document.createElement( enterMode == CKEDITOR.ENTER_P ? 'p' : 'div' ) );
@@ -1125,7 +1115,18 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 					editor.on( 'insertElement', onInsert( doInsertElement ), null, null, 20 );
 					editor.on( 'insertText', onInsert( doInsertText ), null, null, 20 );
 					// Auto fixing on some document structure weakness to enhance usabilities. (#3190 and #3189)
-					editor.on( 'selectionChange', onSelectionChangeFixBody, null, null, 1 );
+					editor.on( 'selectionChange', function()
+					{
+						var sel = editor.getSelection();
+						// Do it only when selection is not locked. (#8222)
+						if ( sel && !sel.isLocked )
+						{
+							var isDirty = editor.checkDirty();
+							onSelectionChangeFixBody.apply( this, arguments );
+							editor.fire( 'updateSnapshot' );
+							!isDirty && editor.resetDirty();
+						}
+					}, null, null, 1 );
 				});
 
 			var titleBackup;
