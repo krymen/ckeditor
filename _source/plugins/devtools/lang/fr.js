85d229b5594123864a62d172b7de6e2576d9bd8f
cksource_robot <cksource_robot@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-11-16 08:16:14 +0000

    Updated translations from CKEditor Translation Center
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7318 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/devtools/lang/fr.js
+++ _source/plugins/devtools/lang/fr.js
@@ -7,10 +7,10 @@ CKEDITOR.plugins.setLang( 'devtools', 'fr',
 {
 	devTools :
 	{
-		title		: 'Information sur l\\\\\'élément',
+		title		: 'Information sur l\'élément',
 		dialogName	: 'Nom de la fenêtre de dialogue',
-		tabName		: 'Nom de l\\\\\'onglet',
-		elementId	: 'ID de l\\\\\'élément',
-		elementType	: 'Type de l\\\\\'élément'
+		tabName		: 'Nom de l\'onglet',
+		elementId	: 'ID de l\'élément',
+		elementType	: 'Type de l\'élément'
 	}
 });
