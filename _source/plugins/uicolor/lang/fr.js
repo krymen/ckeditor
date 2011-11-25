5d0baefdee06223ec568e0f378b724fb2ba93289
cksource_robot <cksource_robot@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-11-25 08:16:43 +0000

    Updated translations from CKEditor Translation Center
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7328 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/uicolor/lang/fr.js
+++ _source/plugins/uicolor/lang/fr.js
@@ -9,7 +9,7 @@ CKEDITOR.plugins.setLang( 'uicolor', 'fr',
 	{
 		title : 'UI Sélecteur de couleur',
 		preview : 'Aperçu',
-		config : 'Collez cette chaîne de caractères dans votre fichier config.js.',
+		config : 'Collez cette chaîne de caractères dans votre fichier config.js',
 		predefined : 'Palettes de couleurs prédéfinies'
 	}
 });
