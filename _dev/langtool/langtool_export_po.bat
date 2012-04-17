69345046bf46dd8a87ed8c475b56d183d6ace070
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-04-17 12:20:17 +0000

    Merged 3.6.x branch [7474] (updated copyright headers)
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7477 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/langtool/langtool_export_po.bat
+++ _dev/langtool/langtool_export_po.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ::
-:: Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+:: Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 :: For licensing, see LICENSE.html or http://ckeditor.com/license
 ::
 :: Generates .po translation files using JavaScript language files as source files.
