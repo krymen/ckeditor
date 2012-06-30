2325a14c57c72e834583be78e7c8d303fa5dc0ea
cksource_robot <cksource_robot@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-06-30 07:15:47 +0000

    Updated translations from CKEditor Translation Center
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7529 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/lang/it.js
+++ _source/lang/it.js
@@ -31,8 +31,8 @@ CKEDITOR.lang['it'] =
 	 * Screenreader titles. Please note that screenreaders are not always capable
 	 * of reading non-English words. So be careful while translating it.
 	 */
-	editorTitle : 'Rich text editor, %1', // MISSING
-	editorHelp : 'Press ALT 0 for help', // MISSING
+	editorTitle : 'Editor di testo Rich Text, %1',
+	editorHelp : 'Premi ALT 0 per aiuto',
 
 	// ARIA descriptions.
 	toolbars	: 'Editor toolbar',
